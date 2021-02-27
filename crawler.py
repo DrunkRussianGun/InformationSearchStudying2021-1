2557e009d8c4cfab692a8b7720c7d3bdbadeeabb
Drunk Russian Gun <drunkrussiangun@gmail.com>
2021-02-27 03:56:36 +0300

    Настроил логирование

--- crawler.py
+++ crawler.py
@@ -1,3 +1,4 @@
+import logging
 import sys
 
 
@@ -6,6 +7,8 @@ def main():
 	if root_page_url is None:
 		return
 
+	configure_logging()
+
 
 def get_root_page_url_or_print_help():
 	if len(sys.argv) != 2:
@@ -13,6 +16,17 @@ def get_root_page_url_or_print_help():
 		return None
 
 	return sys.argv[1]
+
+
+def configure_logging():
+	log_formatter = logging.Formatter("%(asctime)s [%(levelname)s] %(message)s")
+	root_logger = logging.getLogger()
+	root_logger.setLevel(logging.DEBUG)
+
+	console_handler = logging.StreamHandler()
+	console_handler.setFormatter(log_formatter)
+	console_handler.setLevel(logging.INFO)
+	root_logger.addHandler(console_handler)
 
 
 if __name__ == '__main__':
