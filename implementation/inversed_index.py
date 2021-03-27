4b3f1984b90558f08876a481c4261a1ce97aa935
Drunk Russian Gun <drunkrussiangun@gmail.com>
2021-03-27 09:22:58 +0300

    Создал репозиторий для хранения инвертированного индекса

---
+++ implementation/inversed_index.py
@@ -0,0 +1,16 @@
+from tinydb import table
+
+from implementation.infrastructure import get_tinydb_table
+
+
+class InversedIndexRepository:
+	def __init__(self, name):
+		self.__table = get_tinydb_table(name + ".json")
+
+	def get(self):
+		return self.__table.table().get(doc_id = 0)
+
+	def save(self, index):
+		if self.__table.contains(doc_id = 0):
+			self.__table.remove(doc_ids = [0])
+		self.__table.insert(table.Document(index, doc_id = 0))
